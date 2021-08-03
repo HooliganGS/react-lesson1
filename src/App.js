4ed8cca53070234f6c7d9f42ddb7ab60beab3e2e
HooliganGS <gaga.borovikov.ilia12@gmail.com>
2021-08-03 11:51:01 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (<div>
+        <Header/>
+        <div>
+            <input type="text"/>
+        </div>
+        <div className="btn">
+            <button>
+                click me
+            </button>
+            <button>
+                dont click me
+            </button>
+        </div>
+    </div>)
+}
+const Header = () => {
+    return (
+        <div>
+            <div>hello</div>
+            <div>world!</div>
+        </div>
+    )
 }
 
 export default App;
+
