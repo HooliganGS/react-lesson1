71baf1f94fc40dcdb981c61038323e346b50bcc9
HooliganGS <gaga.borovikov.ilia12@gmail.com>
2021-08-27 16:41:22 +0300

    test

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -1,7 +1,7 @@
 import s from './News.module.css'
 const News =()=>{
     return(
-        <div className={s.item}>i am News</div>
+        <div className={s.item}>i am News!</div>
     )
 }
 export default News;
